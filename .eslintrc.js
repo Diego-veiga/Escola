a18a0fc5024441775e933cc931bdb464776261bb
DiegoVeiga <diegoroberto27@hotmail.com>
2021-05-27 21:39:30 -0300

    estruturando o projeto

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'class-methods-use-this': 'off',
+  },
+};
